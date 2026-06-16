0d889d5113261460e01c3c561683cf84e36c1aa5
Kristina <kristina.lysakk@gmail.com>
2026-06-16 22:44:30 +0200

    Mobile Navigation Menu

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+const menuTopNav = document.querySelector("#menuTopNav");
+
+btnOpen.addEventListener("click", openMobileMenu);
+btnClose.addEventListener("click", closeMobileMenu);
+
+function openMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "true");
+}
+
+function closeMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "false");
+}
